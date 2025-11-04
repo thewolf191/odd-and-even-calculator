dab1fa55c5509e9377decc5c5535bb2dd8746fcb
jivitesh lunker <jiviteshlunker@gmail.com>
2025-11-04 22:21:53 +0530

    INIT: First commit of C Odd/Even Calculator project.

---
+++ bin/Debug/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter Num\t");
+    scanf("&d" ,&n);
+    if(n%2 == 0)
+    {
+       printf("even");
+    }
+    else
+    {
+       printf("odd");
+    }
+}
